7753305345538cf854a46a00aed8b727b9ed282b
yuri dandrea <dandreaayuri@gmail.com>
2021-02-25 10:50:31 +0100

    Aggiunta uppercase

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 var nuovoCognome = prompt("inserisci il tuo cognome");
+nuovoCognome = nuovoCognome.charAt(0).toUpperCase() + nuovoCognome.slice(1).toLowerCase();
 cognomi.push(nuovoCognome);
+console.log(nuovoCognome);
 
-// cognomi.sort(function(a, b){
-//   if(a.firstname < b.firstname) { return -1; }
-//   if(a.firstname > b.firstname) { return 1; }
-//   return 0;
-// })
 
 var cognomiOrdinati = cognomi.sort();
 console.log(cognomiOrdinati);
