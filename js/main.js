eebd6c7d0dc877133721d68e0f3177fe37d5abe8
yuri dandrea <dandreaayuri@gmail.com>
2021-02-25 21:26:29 +0100

    Paolo, ho aggiunto la parte mancante.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,17 @@ var cognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
 var nuovoCognome = prompt("inserisci il tuo cognome");
 nuovoCognome = nuovoCognome.charAt(0).toUpperCase() + nuovoCognome.slice(1).toLowerCase();
 cognomi.push(nuovoCognome);
-console.log(nuovoCognome);
-
 
 var cognomiOrdinati = cognomi.sort();
 console.log(cognomiOrdinati);
 
-console.log(cognomi.indexOf(nuovoCognome) + 1);
+// console.log(cognomi.indexOf(nuovoCognome) + 1);
+
+var posizioneUmana;
+
+for (i = 0; i < cognomiOrdinati.length; i++) {
+  if (nuovoCognome == cognomiOrdinati[i]) {
+    posizioneUmana= ++i;
+    console.log(posizioneUmana);
+  }
+}
